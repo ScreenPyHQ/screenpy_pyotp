36e7dd64292497378ce04a6e8dbadc5ee058314e
Marcel Wilson <trenchrats@gmail.com>
2024-02-15 17:08:04 -0600

    ruff ANN

--- tests/test_abilities.py
+++ tests/test_abilities.py
@@ -6,15 +6,15 @@ from screenpy_pyotp.abilities import AuthenticateWith2FA
 
 
 class TestAuthenticateWith2FA:
-    def test_can_be_instantiated(self):
+    def test_can_be_instantiated(self, mocked_pyotp: mock.Mock) -> None:
         a1 = AuthenticateWith2FA.using_secret("")
-        a2 = AuthenticateWith2FA.using(None)
+        a2 = AuthenticateWith2FA.using(mocked_pyotp)
 
         assert isinstance(a1, AuthenticateWith2FA)
         assert isinstance(a2, AuthenticateWith2FA)
 
     @mock.patch("screenpy_pyotp.abilities.authenticate_with_2fa.pyotp")
-    def test_using_secret(self, mocked_pyotp):
+    def test_using_secret(self, mocked_pyotp: mock.Mock) -> None:
         secret = "the macguffin"
         AuthenticateWith2FA.using_secret(secret)
 
